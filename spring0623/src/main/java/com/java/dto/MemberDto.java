9cf8a49d15a84b7c078796bad7c4da268d9f2fd5
005 <005@DESKTOP-9H7317A>
2023-06-23 17:21:15 +0900

    6월 23일 회원리스트,회원view

--- spring0623/src/main/java/com/java/dto/MemberDto.java
+++ spring0623/src/main/java/com/java/dto/MemberDto.java
@@ -11,7 +11,7 @@ public class MemberDto {
 	private String id;
 	private String pw;
 	private String name;
-	private String job;
+	private String phone;
 	private String gender;
 	private String hobby;
 
