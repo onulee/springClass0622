0b9aabe5a2f26ef719044fc50c430fbbfa4f9a8f
005 <005@DESKTOP-9H7317A>
2023-07-17 12:42:15 +0900

    login부분 진행

---
+++ spring0717_board/src/main/java/com/java/mapper/MemberMapper.java
@@ -0,0 +1,15 @@
+package com.java.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.java.dto.MemberDto;
+
+@Mapper
+public interface MemberMapper {
+
+	//회원 1명 가져오기(로그인)
+	MemberDto selectOne(String id, String pw);
+
+	
+	
+}
